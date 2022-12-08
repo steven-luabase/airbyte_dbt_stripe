c06312cada8438bc9fd3647c5f6a339afbc62bcf
Steven Wang <steven@luabase.com>
2022-12-07 19:46:02 -0800

    break out cte

--- models/stripe__daily_overview.sql
+++ models/stripe__daily_overview.sql
@@ -28,23 +28,8 @@ customer_stats as (
     from {{ ref('int_stripe__daily_customer_stats') }}
 ),
 
-filtered_subs as (
-    select subscription_payments.subscription_id,
-        subscription_payments.date,
-        subscription_id,
-        plan_amount,
-        status,
-        customer_email,
-        canceled_at
-    from
-        subscription_payments
-    where
-        subscription_payments.date <= date_trunc('day', dt.date)
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY subscription_id ORDER BY date) = 1
-),
-
-sub_stats as (
-    select
+churned_stats as (
+     select
         {{ dbt_utils.date_trunc("day", 'dt.date') }} as date,
         (
             /*
@@ -60,99 +45,19 @@ sub_stats as (
                     ) 
                     then 1 end
                 ) as churned_subscriptions
-            from filtered_subs
-        ),
-        (
-            /*
-            New subscriptions are counted on the first issued invoice.
-            */
-            select
-                count(
-                    case when (
-                        (
-                            filtered_subs.status = 'active'
-                            or filtered_subs.status = 'past due'
-                            or filtered_subs.status = 'canceled'
-                        )
-                        and filtered_subs.invoice_number = 1
-                        and filtered_subs.customer_email is not null
-                    )
-                    then 1 end
-                ) as new_subscriptions
-            from filtered_subs
-        ),
-        (
-            /*
-            Active subscriptions are counted when the status is active or past due.
-            If the subscription is canceled, the sub will be counted only if the current
-            day is less than the day the sub was canceled.
-            */
-            select
-                count(
-                    case when (
-                        (
-                            filtered_subs.status = 'active'
-                            or filtered_subs.status = 'past due'
-                            or (
-                                filtered_subs.status = 'canceled'
-                                and dt.date < date_trunc('day', filtered_subs.canceled_at)
-                            )
-                        )
-                        and filtered_subs.customer_email is not null
-                    ) 
-                    then 1 end
-                ) as active_subscriptions
-            from filtered_subs
-        ),
-        /*
-        There are multiple different values that can be used when summing mrr. Stripe
-        seems to use the average value of the subscription over its entire lifetime.
-        */
-        (
-            select
-                coalesce(round(sum(
-                    case when (
-                        filtered_subs.status = 'canceled'
-                        and dt.date = date_trunc('day', filtered_subs.canceled_at)
-                        and filtered_subs.customer_email is not null
-                    ) 
-                    then filtered_subs.plan_amount / 100 end
-                ), 2), 0) as churned_mrr
-            from filtered_subs
-        ),
-        (
-            select
-                coalesce(round(sum(
-                    case when (
-                        (
-                            filtered_subs.status = 'active'
-                            or filtered_subs.status = 'past due'
-                            or filtered_subs.status = 'canceled'
-                        )
-                        and filtered_subs.invoice_number = 1
-                        and filtered_subs.customer_email is not null
-                    ) 
-                    then filtered_subs.plan_amount / 100 end
-                ), 2), 0) as "new_mrr"
-            from filtered_subs
-        ),
-        (
-            select
-                coalesce(round(sum(
-                    case when (
-                        (
-                            filtered_subs.status = 'active'
-                            or filtered_subs.status = 'past due'
-                            or (
-                                filtered_subs.status = 'canceled'
-                                and dt.date < date_trunc('day', filtered_subs.canceled_at)
-                            )
-                        )
-                        and filtered_subs.customer_email is not null
-                    ) 
-                    then filtered_subs.plan_amount / 100 end
-                ), 2), 0) as mrr
-            from filtered_subs
+            from (
+                select subscription_payments.subscription_id,
+                    subscription_payments.date,
+                    subscription_id,
+                    status,
+                    customer_email,
+                    canceled_at
+                from
+                    subscription_payments
+                where
+                    subscription_payments.date <= date_trunc('day', dt.date)
+                QUALIFY ROW_NUMBER() OVER (PARTITION BY subscription_id ORDER BY date) = 1
+            ) as filtered_subs
         )
     from (
         select
@@ -162,17 +67,212 @@ sub_stats as (
         ) as dt
 ),
 
+-- sub_stats as (
+--     select
+--         {{ dbt_utils.date_trunc("day", 'dt.date') }} as date,
+--         (
+--             /*
+--             Churned subscriptions are counted when the current day is
+--             less than the day the subscription was canceled.
+--             */
+--             select
+--                 count(
+--                     case when (
+--                         filtered_subs.status = 'canceled'
+--                         and dt.date = date_trunc('day', filtered_subs.canceled_at)
+--                         and filtered_subs.customer_email is not null
+--                     ) 
+--                     then 1 end
+--                 ) as churned_subscriptions
+--             from (
+--                 select subscription_payments.subscription_id,
+--                     subscription_payments.date,
+--                     subscription_id,
+--                     status,
+--                     customer_email,
+--                     canceled_at
+--                 from
+--                     subscription_payments
+--                 where
+--                     subscription_payments.date <= date_trunc('day', dt.date)
+--                 QUALIFY ROW_NUMBER() OVER (PARTITION BY subscription_id ORDER BY date) = 1
+--             ) as filtered_subs
+--         ),
+--         (
+--             /*
+--             New subscriptions are counted on the first issued invoice.
+--             */
+--             select
+--                 count(
+--                     case when (
+--                         (
+--                             filtered_subs.status = 'active'
+--                             or filtered_subs.status = 'past due'
+--                             or filtered_subs.status = 'canceled'
+--                         )
+--                         and filtered_subs.invoice_number = 1
+--                         and filtered_subs.customer_email is not null
+--                     )
+--                     then 1 end
+--                 ) as new_subscriptions
+--             from (
+--                 select subscription_payments.subscription_id,
+--                     subscription_payments.date,
+--                     subscription_id,
+--                     invoice_number,
+--                     status,
+--                     customer_email,
+--                     canceled_at
+--                 from
+--                     subscription_payments
+--                 where
+--                     subscription_payments.date = date_trunc('day', dt.date)
+--                 QUALIFY ROW_NUMBER() OVER (PARTITION BY subscription_id ORDER BY date) = 1
+--             ) as filtered_subs
+--         ),
+--         (
+--             /*
+--             Active subscriptions are counted when the status is active or past due.
+--             If the subscription is canceled, the sub will be counted only if the current
+--             day is less than the day the sub was canceled.
+--             */
+--             select
+--                 count(
+--                     case when (
+--                         (
+--                             filtered_subs.status = 'active'
+--                             or filtered_subs.status = 'past due'
+--                             or (
+--                                 filtered_subs.status = 'canceled'
+--                                 and dt.date < date_trunc('day', filtered_subs.canceled_at)
+--                             )
+--                         )
+--                         and filtered_subs.customer_email is not null
+--                     ) 
+--                     then 1 end
+--                 ) as active_subscriptions
+--             from (
+--                 select subscription_payments.subscription_id,
+--                     subscription_payments.date,
+--                     subscription_id,
+--                     status,
+--                     customer_email,
+--                     canceled_at
+--                 from
+--                     subscription_payments
+--                 where
+--                     subscription_payments.date <= date_trunc('day', dt.date)
+--                 QUALIFY ROW_NUMBER() OVER (PARTITION BY subscription_id ORDER BY date) = 1
+--             ) as filtered_subs
+--         ),
+--         /*
+--         There are multiple different values that can be used when summing mrr. Stripe
+--         seems to use the average value of the subscription over its entire lifetime.
+--         */
+--         (
+--             select
+--                 coalesce(round(sum(
+--                     case when (
+--                         filtered_subs.status = 'canceled'
+--                         and dt.date = date_trunc('day', filtered_subs.canceled_at)
+--                         and filtered_subs.customer_email is not null
+--                     ) 
+--                     then filtered_subs.plan_amount / 100 end
+--                 ), 2), 0) as churned_mrr
+--             from (
+--                 select subscription_payments.subscription_id,
+--                     subscription_payments.date,
+--                     subscription_id,
+--                     plan_amount,
+--                     status,
+--                     customer_email,
+--                     canceled_at
+--                 from
+--                     subscription_payments
+--                 where
+--                     subscription_payments.date <= date_trunc('day', dt.date)
+--                 QUALIFY ROW_NUMBER() OVER (PARTITION BY subscription_id ORDER BY date) = 1
+--             ) as filtered_subs
+--         ),
+--         -- (
+--         --     select
+--         --         coalesce(round(sum(
+--         --             case when (
+--         --                 (
+--         --                     filtered_subs.status = 'active'
+--         --                     or filtered_subs.status = 'past due'
+--         --                     or filtered_subs.status = 'canceled'
+--         --                 )
+--         --                 and filtered_subs.invoice_number = 1
+--         --                 and filtered_subs.customer_email is not null
+--         --             ) 
+--         --             then filtered_subs.plan_amount / 100 end
+--         --         ), 2), 0) as "new_mrr"
+--         --     from (
+--         --         select subscription_payments.subscription_id,
+--         --             subscription_payments.date,
+--         --             subscription_id,
+--         --             invoice_number,
+--         --             plan_amount,
+--         --             status,
+--         --             customer_email
+--         --         from
+--         --             subscription_payments
+--         --         where
+--         --             subscription_payments.date = date_trunc('day', dt.date)
+--         --         QUALIFY ROW_NUMBER() OVER (PARTITION BY subscription_id ORDER BY date) = 1
+--         --     ) as filtered_subs
+--         -- ),
+--         (
+--             select
+--                 coalesce(round(sum(
+--                     case when (
+--                         (
+--                             filtered_subs.status = 'active'
+--                             or filtered_subs.status = 'past due'
+--                             or (
+--                                 filtered_subs.status = 'canceled'
+--                                 and dt.date < date_trunc('day', filtered_subs.canceled_at)
+--                             )
+--                         )
+--                         and filtered_subs.customer_email is not null
+--                     ) 
+--                     then filtered_subs.plan_amount / 100 end
+--                 ), 2), 0) as mrr
+--             from (
+--                 select subscription_payments.subscription_id,
+--                     subscription_payments.date,
+--                     subscription_id,
+--                     plan_amount,
+--                     status,
+--                     customer_email,
+--                     canceled_at
+--                 from
+--                     subscription_payments
+--                 where
+--                     subscription_payments.date <= date_trunc('day', dt.date)
+--                 QUALIFY ROW_NUMBER() OVER (PARTITION BY subscription_id ORDER BY date) = 1
+--             ) as filtered_subs
+--         )
+--     from (
+--         select
+--             date
+--         from
+--             daily_transactions
+--         ) as dt
+-- ),
+
 daily_overview as (
     select
         *,
-        coalesce(round(mrr/nullif(active_subscriptions, 0.0), 2), 0.0) as mrr_per_subscription,
-        coalesce(round(mrr/nullif(active_customers, 0.0), 2), 0.0) as mrr_per_customer,
+        -- coalesce(round(mrr/nullif(active_subscriptions, 0.0), 2), 0.0) as mrr_per_subscription,
+        -- coalesce(round(mrr/nullif(active_customers, 0.0), 2), 0.0) as mrr_per_customer,
         coalesce(active_customers - lag(active_customers, 1) over (order by date), 0) as customers_diff,
-        coalesce(active_subscriptions - lag(active_subscriptions, 1) over (order by date), 0) as subscriptions_diff,
-        coalesce(mrr - lag(mrr, 1) over (order by date), 0.0) as mrr_diff
+        -- coalesce(active_subscriptions - lag(active_subscriptions, 1) over (order by date), 0) as subscriptions_diff,
+        -- coalesce(mrr - lag(mrr, 1) over (order by date), 0.0) as mrr_diff
     from
         daily_transactions
-        left join sub_stats
+        left join churned_stats
             using(date)
         left join customer_stats
             using(date)
